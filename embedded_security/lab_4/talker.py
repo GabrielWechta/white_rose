c3f0f62cd660e3b31b4be1be102f0d8d942197a7
GabrielWechta <wechtagabriel@gmail.com>
2023-01-17 09:53:58 +0100

    Introduce changes to the talker.py

--- embedded_security/lab_4/talker.py
+++ embedded_security/lab_4/talker.py
@@ -1,4 +1,5 @@
 import threading
+from itertools import product
 from time import time, sleep
 from datetime import datetime
 
@@ -15,8 +16,8 @@ class MySerialWorker(object):
     def send_command(self, cmd):
         self.pending_commands.append(cmd)
 
-    def query_command(self, start_time, fd, cmd, callback):
-        self.pending_queries.append([start_time, fd, cmd, callback])
+    def query_command(self, start_time, fd, result_dict, cmd, callback):
+        self.pending_queries.append([start_time, fd, result_dict, cmd, callback])
 
     def _do_query(self, cmd):
         self.ser.write(cmd.encode())
@@ -46,48 +47,76 @@ class MySerialWorker(object):
 
 class MainProgram():
     def __init__(self):
-        self.ser = MySerialWorker('/dev/ttyACM0', baudrate=9600, timeout=2)
+        self.ser = MySerialWorker('/dev/ttyACM0', baudrate=9600, timeout=0.1)
         self.filename = datetime.now().strftime("%H:%M:%S") + ".txt"
         self.pending_command = False
 
-    def on_result(self, start_time, fd, cmd, result):
+    def on_result(self, start_time, fd, result_dict, cmd, result):
         clean_cmd = cmd.replace("\n", "")
         exec_time = time() - start_time
         print(f"Device responded to {clean_cmd} with {result} in {exec_time}")
-        fd.write(f"{exec_time}:{clean_cmd}->{result}\n")
+        if fd is not None:
+            fd.write(f"{exec_time},{clean_cmd},{result}\n")
+        if result_dict is not None:
+            result_dict[clean_cmd] = {"result": result, "exec_time": exec_time}
         self.pending_command = False
 
-    def talk_to_device(self, commands_generator):
+    def send_recv_procedure(self, cmds, result_dict=None, fd=None):
         i = 0
-        self.ser.run_threaded()
-        with open(self.filename, 'w') as opened_file:
-            self.pending_command = True
-            self.ser.query_command(start_time=0.0, fd=opened_file, cmd=" \n", callback=self.on_result)
-            sleep(1)  # getting init message from arduino
-            cmds = commands_generator()
-            while True:
-                if self.pending_command is False:
-                    if i >= len(cmds):
-                        self.ser.terminate()
-                        break
-                    self.pending_command = True
-                    cmd = cmds[i]
-                    self.ser.query_command(start_time=time(), fd=opened_file, cmd=cmd + "\n", callback=self.on_result)
-                    i += 1
+        self.pending_command = True
+        self.ser.query_command(start_time=0.0, fd=fd, result_dict=result_dict, cmd=" \n", callback=self.on_result)
+        sleep(1)  # getting init message from arduino
+        while True:
+            if self.pending_command is False:
+                if i >= len(cmds):
+                    self.ser.terminate()
+                    break
+                self.pending_command = True
+                cmd = cmds[i]
+                self.ser.query_command(start_time=time(), fd=fd, result_dict=result_dict, cmd=cmd + "\n",
+                                       callback=self.on_result)
+                i += 1
 
-                sleep(0.01)
+            sleep(0.01)
+
+    def talk_to_device(self, cmds, save_to_file):
+        self.ser.run_threaded()
+        if save_to_file is True:
+            with open(self.filename, 'w') as opened_file:
+                self.send_recv_procedure(cmds=cmds, fd=opened_file)
+        else:
+            self.send_recv_procedure(cmds=cmds)
+
+    def perform_timing_attack(self):
+        for _ in range(4):
+            cmds_suffixes = get_sa()
+            timing_result_dict = {}
+            cmds = []
+            self.ser.run_threaded()
+            with open(self.filename, 'w') as opened_file:
+                self.send_recv_procedure(cmds=cmds, fd=opened_file, result_dict=timing_result_dict)
 
 
 def get_sa():
     cmds = [f"{chr(i)}s" for i in range(32, 126)]
     return cmds
 
+
 def get_l():
     cmds = ['?s', ' ']
     return cmds
 
+
 def generate_all_one_byte_commands():
     cmds = [chr(i) for i in range(256)]
+    return cmds
+
+
+def generate_pin(length):
+    combinations = list(product(range(0, 10), repeat=length))
+    cmds = ["".join(str(x) for x in comb) for comb in combinations]
+    cmds = ["01" + suf for suf in cmds]
+    print(cmds)
     return cmds
 
 
@@ -98,4 +127,4 @@ def generate_all_two_byte_commands():
 
 if __name__ == "__main__":
     talker = MainProgram()
-    talker.talk_to_device(commands_generator=generate_all_one_byte_commands)
+    talker.talk_to_device(cmds=generate_pin(3), save_to_file=True)
