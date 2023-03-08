73c336a1f2e6290b106181ad8bbe37a1acdb6ce3
GabrielWechta <wechtagabriel@gmail.com>
2023-03-08 14:38:02 +0100

    Add AD Task 2

--- distributed_algorithms/labs/list_1/leader_election_utils.py
+++ distributed_algorithms/labs/list_1/leader_election_utils.py
@@ -1,5 +1,19 @@
+import math
 import random
 
+from matplotlib import pyplot as plt
+
+
+def known_n_p_generator(nodes_num: int):
+    while True:
+        yield 1 / nodes_num
+
+
+def known_u_p_generator(nodes_num: int):
+    m = math.ceil(math.log2(nodes_num))
+    while True:
+        for i in range(1, m + 1):
+            yield (1 / 2) ** i
 
 class Channel:
     def __init__(self):
@@ -49,3 +63,54 @@ class Node:
             # print(f"{self.i}:{self.node_id}")
             self.channel.beep(slot_ind=self.i, node_id=self.node_id)
         self.i += 1
+
+
+
+class LSimulator:
+    def __init__(self, number_of_experiments: int):
+        self.number_of_experiments = number_of_experiments
+
+    def get_L_known_n(self, n: int):
+        L_value = self.perform_experiment(nodes_num=n, p_generator=known_n_p_generator)
+        return L_value
+
+    def get_L_known_u(self, u: int):
+        n = random.randint(2, u)
+        L_value = self.perform_experiment(nodes_num=n, p_generator=known_u_p_generator)
+        return L_value
+
+    @staticmethod
+    def perform_experiment(nodes_num: int, p_generator):
+        channel = Channel()
+        nodes = [Node(node_id=node_id, channel=channel, p_generator_ref=p_generator(nodes_num=nodes_num)) for node_id in
+                 range(nodes_num)]
+        while True:
+            for node in nodes:
+                node.election()
+            if channel.exists_single():
+                break
+            channel.extend_slots()
+        return channel.get_slots_length()
+
+    def plot_histogram(self, n: int = None, u: int = None):
+        if n is None and u is None:
+            raise ValueError("Specify `n` or `u`.")
+        if n is not None:
+            experiment_results = []
+            for _ in range(self.number_of_experiments):
+                experiment_results.append(self.get_L_known_n(n=n))
+            bins = sorted(set(experiment_results))
+            plt.hist(experiment_results, bins=bins, alpha=0.7, align='left', label=f"{n=}")
+        if u is not None:
+            experiment_results = []
+            for _ in range(self.number_of_experiments):
+                experiment_results.append(self.get_L_known_u(u=u))
+            bins = sorted(set(experiment_results))
+            plt.hist(experiment_results, bins=bins, alpha=0.7, align='left', label=f"{u=}")
+
+        plt.xlabel('Number of slots')
+        plt.ylabel('Number of results')
+        plt.title('Histogram of the random variable L')
+        plt.xticks(bins, bins)
+        plt.legend(loc='upper right')
+        plt.show()
